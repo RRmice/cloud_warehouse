d6d877ee270225f9c263490817b83e6e355afe2c
rrmice <rrmice@yandex.ru>
2019-07-11 13:39:23 +0300

    Init commit

--- client/src/main/java/MainClient.java
+++ client/src/main/java/MainClient.java
@@ -4,20 +4,16 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
-
+public class MainClient extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
-
-        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
-        primaryStage.setTitle("Hello World");
-
-        Scene mainScene = new Scene(root, 300, 275);
-
-        primaryStage.setScene(mainScene);
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/main.fxml"));
+        Parent root = fxmlLoader.load();
+        primaryStage.setTitle("Box Client");
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
